0f1353e8dc98d5c749a70c8f6268556196e07be5
skochauf <stephan.kochauf@lv.campus02.at>
2022-11-25 16:10:24 +0100

    Hase

--- src/org/campus02/hasen/DemoApp.java
+++ src/org/campus02/hasen/DemoApp.java
@@ -10,7 +10,7 @@ public class DemoApp {
         hase.verteilen();
 
 
-        WeihnachtsHase santa = new WeihnachtsHase("Santa");
+        WeihnachtsHase santa = new WeihnachtsHase("Santa", "Mutter");
         santa.fressen();
         santa.schlafen();
         santa.verteilen();
@@ -20,6 +20,26 @@ public class DemoApp {
         felix.schlafen();
         felix.verteilen();
 
+        System.out.println("------");
+
+        Object o = santa;
+        Hase h1 = santa;
+        System.out.println("h1.verteilen");
+        h1.verteilen();
+        System.out.println(o.toString());
+
+        System.out.println(santa.getBestFriend());
+        h1.verteilen();
+
+        Hasenstall hoppelWiese = new Hasenstall();
+        hoppelWiese.add(hase);
+        hoppelWiese.add(santa);
+        hoppelWiese.add(felix);
+
+        System.out.println("--- verteilen von Hoppelwiese ---");
+        hoppelWiese.verteilen();
+
+        hoppelWiese.fuettern();
 
     }
 }
--- src/org/campus02/hasen/WeihnachtsHase.java
+++ src/org/campus02/hasen/WeihnachtsHase.java
@@ -2,12 +2,33 @@ package org.campus02.hasen;
 
 public class WeihnachtsHase extends Hase {
 
-    public WeihnachtsHase(String name) {
+    public String bestFriend;
+
+    public WeihnachtsHase(String name, String bestFriend) {
         super(name);
+        this.bestFriend = bestFriend;
+    }
+
+    @Override
+    public String toString() {
+        return "WeihnachtsHase{" +
+                "bestFriend='" + bestFriend + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getBestFriend() {
+        return bestFriend;
     }
 
     @Override
     public void verteilen() {
-        System.out.println(name + " verteilt die Geschenke unter dem Christbaum");
+        System.out.println(name + " verteilt die Geschenke unter dem Christbaum. "
+                + bestFriend + " hilft beim Verteilen");
+    }
+
+    public void legeGeschenkeUntermChristbaum() {
+        System.out.println(name + " verteilt die Geschenke unter dem Christbaum. "
+                + bestFriend + " hilft beim Verteilen");
     }
 }
